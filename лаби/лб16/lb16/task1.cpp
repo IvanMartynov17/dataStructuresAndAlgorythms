0e844f0432049a25f2c79f500ced05897a982f85
Ivan Martynov <martynovivan611@gmail.com>
2026-05-05 19:12:32 +0300

    task1

--- лаби/лб16/lb16/task1.cpp
+++ лаби/лб16/lb16/task1.cpp
@@ -129,7 +129,7 @@ void binarySearch(vector<Student> &students) {
 
 if (found == -1) {
     cout << "Student with " << fixed
-    << setprecision(2) << target << " Not found." << endl;
+    << setprecision(2) << target << " Not found.\n";
     return;
 }
     int left = found, right = found;
